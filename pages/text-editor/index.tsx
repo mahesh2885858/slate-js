fad6d20c4d558d78099fa0e2ab8b3a9b21b9f3cd
mahesh <mahesh2885858@gmail.com>
2022-08-31 17:50:36 +0530

    from scratch again

--- pages/text-editor/index.tsx
+++ pages/text-editor/index.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useCallback, useMemo } from 'react'
-import { createEditor } from 'slate'
-import { Slate, Editable, withReact, } from 'slate-react'
-import { withHistory } from "slate-history"
-import { BaseEditor, Descendant, Editor, Transforms, Text } from 'slate'
-import { ReactEditor, RenderElementProps } from 'slate-react'
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
-type CustomText = { text: string }
+
+import React, { useMemo } from 'react'
+import { Slate, withReact, Editable, ReactEditor, RenderElementProps, RenderLeafProps } from 'slate-react'
+import { BaseEditor, createEditor, Descendant, Editor, Transforms, Text, Element, Range } from 'slate'
+import { withHistory } from 'slate-history'
+type ElementType = "paragraph" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "link" | "image" | "table"
+type CustomElement = { type: ElementType, children: CustomText[] }
+type CustomText = { [key: string]: string | boolean }
+
 declare module 'slate' {
     interface CustomTypes {
         Editor: BaseEditor & ReactEditor
@@ -14,98 +15,37 @@ declare module 'slate' {
     }
 }
 const Index = () => {
-    const editor = useMemo(() => withReact(withHistory(createEditor())), []);
-    const [value, setValue] = useState<Descendant[]>([
-        {
-            type: 'paragraph',
+    const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+    const RenderElement = (props: RenderElementProps) => {
+        const { attributes, children, element } = props
+        switch (element.type) {
+            case "paragraph":
 
-            children: [{ text: 'This is my paragraph!' }]
-        }
-    ])
-
-    const Leaf = (props: any) => {
-        return (
-            <span
-                {...props.attributes}
-                style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
-            >
-                {props.children}
-            </span>
-        )
-    }
-
-
-
-
-    const DefaultElement = (props: RenderElementProps) => {
-
-        return <p {...props.attributes}>{props.children}</p>
-    }
-
-
-    const CustomEditor = {
-        isBoldMarkActive(editor: BaseEditor & ReactEditor) {
-
-            const [match] = Editor.nodes(editor, {
-                match: (n) => {
-                    return n.bold === true
-                },
-                universal: true,
-            })
-
-            return !!match
-        },
-
-
-
-
-        toggleBoldMark(editor: BaseEditor & ReactEditor) {
-
-            const isActive = CustomEditor.isBoldMarkActive(editor)
-
-            Transforms.insertNodes(editor, { type: "paragraph", children: [{ text: "mahesh inserted the test here" }] })
-        },
-
-
-
-
-    }
-    const makeBold = () => {
-        CustomEditor.toggleBoldMark(editor)
-    }
-
-    const renderElement = useCallback((props: any) => {
-        switch (props.element.type) {
+                return <p {...attributes} >{children}</p>
+            case "h1":
+                return <h1 {...attributes} className="text-3xl font-bold">{children}</h1>
             default:
-                return <DefaultElement {...props} />
+                break;
         }
-    }, [])
-    const renderLeaf = useCallback((props: any) => {
-
-        return <Leaf {...props} />
-    }, [])
+        return <h1 {...props.attributes}>{props.children}</h1>
+    }
+    const RenderLeaf = (props: RenderLeafProps) => {
+        let { attributes, children, leaf, text } = props
+        if (leaf.red) {
+            children = <span className='text-red-500'>{children}</span>
+        }
+        return <span {...props}>{children}</span>
+    }
     return (
-        <div className='w-screen  flex justify-center items-center h-screen bg-blue-500'>
+        <Slate editor={editor} value={[{ type: "paragraph", children: [{ text: "test" }, { text: "test2", red: true }] }, { type: "h1", children: [{ text: "mahesh" }] }]}>
+            <Editable
+                renderElement={RenderElement}
+                renderLeaf={RenderLeaf}
+                autoFocus
 
-            <div className='w-[45%] h-[80%] bg-white rounded-md overflow-hidden p-2'>
-                <div className=' toolbar w-full flex items-center gap-2 h-[50px] border  '>
-                    <div onMouseDown={makeBold} className='bg-blue-300 p-1 font-bold px-4 cursor-pointer hover:bg-blue-500 '>Test</div>
+            />
 
-                </div>
-                <div className='h-[calc(100%-50px)] overflow-y-auto border border-black'>
-                    <Slate editor={editor} value={value} onChange={(val) => {
-                        console.log(val)
-                    }} >
-                        <Editable
-                            renderElement={renderElement}
-                            renderLeaf={renderLeaf} />
-                    </Slate>
-                </div>
-            </div>
-
-
-
-        </div>
+        </Slate>
     )
 }
 
